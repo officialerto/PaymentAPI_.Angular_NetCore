6983a8b60665e2df0f8f06476602ab0824981331
Ertugrul <57365047+officialerto@users.noreply.github.com>
2023-08-21 00:03:15 +0300

    PaymentAPI-Angular-.NetCore

---
+++ PaymentAPI/PaymentAPI/PaymentAPI/Models/PaymentDetail.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string CardOwnerName { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string CardNumber { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string ExpirationDate { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string SecurityCode { get; set; } = "";
+    }
+
+}
